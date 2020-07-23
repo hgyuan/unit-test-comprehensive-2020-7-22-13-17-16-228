57b2acd9c14579548f85816a0b7ecf03af3fce04
1764775976@qq.com <1764775976@qq.com>
2020-07-23 19:04:21 +0800

    [unit-test guess-game][richard]feature :should_return_2A0B_when_guess_given_number_1234_answer_1256

--- src/main/java/example/NumberGuess.java
+++ src/main/java/example/NumberGuess.java
@@ -6,14 +6,14 @@ public class NumberGuess {
     public String guess(int[] guess, int[] answer) {
         int isCorrect = 0;
         for (int index = 0; index < guess.length; index++) {
-            if (guess[index] != answer[index]) {
+            if (guess[index] == answer[index]) {
                 isCorrect++;
             }
         }
-        if(isCorrect==4){
+        if(isCorrect==0){
             return "0A0B";
         }
-        return "4A0B";
+        return String.format("%sA0B",isCorrect);
     }
 
 }
