d0c3ea6215c9e3894ed1fa93fac99c0f6a3c8ca2
1764775976@qq.com <1764775976@qq.com>
2020-07-23 19:18:47 +0800

    [unit-test guess-game][richard]refactor : merge the function calculate the isCorrectPosition and isCorrectNumber.

--- src/main/java/example/NumberGuess.java
+++ src/main/java/example/NumberGuess.java
@@ -6,13 +6,11 @@ public class NumberGuess {
     public String guess(int[] guess, int[] answer) {
         int isCorrectPosition = 0;
         int isCorrectNumber = 0;
-        for (int index = 0; index < guess.length; index++) {
-            if (guess[index] == answer[index]) {
-                isCorrectPosition++;
-            }
-        }
         for(int index =0;index<guess.length;index++){
             for(int answerIndex=0;answerIndex<answer.length;answerIndex++){
+                if(guess[index]==answer[index]&&index==answerIndex){
+                    isCorrectPosition++;
+                }
                 if(guess[index]==answer[answerIndex]){
                     isCorrectNumber++;
                 }
