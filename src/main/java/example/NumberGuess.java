8374aebcda967e2274e1aa43888a95e84ea9ce6a
1764775976@qq.com <1764775976@qq.com>
2020-07-23 18:56:22 +0800

    [unit-test guess-game][richard]feature :should_return_0A0B_when_guess_given_guess_number_6789_answer_1234

--- src/main/java/example/NumberGuess.java
+++ src/main/java/example/NumberGuess.java
@@ -3,11 +3,15 @@ package example;
 public class NumberGuess {
 
 
-    public String guess(int[] guess,int[] answer) {
-        for(int index =0 ;index<guess.length;index++){
-            if(guess[index]!=answer[index]){
-                return "";
+    public String guess(int[] guess, int[] answer) {
+        int isCorrect = 0;
+        for (int index = 0; index < guess.length; index++) {
+            if (guess[index] != answer[index]) {
+                isCorrect++;
             }
+        }
+        if(isCorrect==4){
+            return "0A0B";
         }
         return "4A0B";
     }
