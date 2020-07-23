2757db403812bab79307fcbc415636e42c296c8d
1764775976@qq.com <1764775976@qq.com>
2020-07-23 18:49:27 +0800

    [unit-test guess-game][richard]feature :should_return_4A0B_when_guess_given_guess_number_1234_answer_1234

--- src/main/java/example/NumberGuess.java
+++ src/main/java/example/NumberGuess.java
@@ -4,7 +4,12 @@ public class NumberGuess {
 
 
     public String guess(int[] guess,int[] answer) {
-        return null;
+        for(int index =0 ;index<guess.length;index++){
+            if(guess[index]!=answer[index]){
+                return "";
+            }
+        }
+        return "4A0B";
     }
 
 }
