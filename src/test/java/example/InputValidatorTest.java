1f753b3ad705dc282f9cd296fce4d593759353ce
1764775976@qq.com <1764775976@qq.com>
2020-08-02 13:28:16 +0800

    [unit-test guess-game][richard]test: should_return_false_when_is_length_valid_given_array_length_1

--- src/test/java/example/InputValidatorTest.java
+++ src/test/java/example/InputValidatorTest.java
@@ -2,6 +2,7 @@ package example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InputValidatorTest {
@@ -13,5 +14,15 @@ public class InputValidatorTest {
         boolean result = inputValidator.isLengthValid(str);
 
         assertTrue(result);
+    }
+
+    @Test
+    void should_return_false_when_is_length_valid_given_array_length_1() {
+        String[] str = new String[1];
+        InputValidator inputValidator = new InputValidator();
+
+        boolean result = inputValidator.isLengthValid(str);
+
+        assertFalse(result);
     }
 }
